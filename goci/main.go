f20b7fb02e69af03c5764fd6d26b8e2340852d4d
Ryan Bradshaw <ryan@rbradshaw.dev>
2022-06-08 13:22:23 +0000

    goci gitrepo

--- goci/main.go
+++ goci/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 type executer interface {
@@ -16,7 +17,7 @@ func run(proj string, out io.Writer) error {
 		return fmt.Errorf("project directory is required: %w", ErrValidation)
 	}
 
-	pipeline := make([]executer, 3)
+	pipeline := make([]executer, 4)
 
 	pipeline[0] = newStep(
 		"go build",
@@ -40,6 +41,15 @@ func run(proj string, out io.Writer) error {
 		"Gofmt: SUCCESS",
 		proj,
 		[]string{"-l", "."},
+	)
+
+	pipeline[3] = newTimeoutStep(
+		"git push",
+		"git",
+		"Git Push: SUCCESS",
+		proj,
+		[]string{"push", "origin", "master"},
+		10*time.Second,
 	)
 
 	for _, s := range pipeline {
